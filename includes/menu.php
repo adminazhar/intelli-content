5ad2a0b0614cac6347c16c1c453e4d0fc72e0800
adminazhar <admin@azhark.com>
2024-02-15 15:19:33 +0530

    update menu to expand title

--- includes/menu.php
+++ includes/menu.php
@@ -4,13 +4,23 @@
 function intelli_content_add_menu() {
     // Add main menu item for IntelliContent
     add_menu_page(
-        'Intelli Content',            // Page title
-        'Intelli Content',            // Menu title
-        'manage_options',             // Capability required
-        'intelli-content-generation', // Menu slug for Content Generation page
+        'Intelli Content',              // Page title
+        'Intelli Content',              // Menu title
+        'manage_options',               // Capability required
+        'intelli-content-generation',   // Menu slug for Content Generation page
         'intelli_content_generation_page', // Callback function for Content Generation page
-        'dashicons-admin-generic',    // Icon
-        20                            // Position
+        'dashicons-admin-generic',      // Icon
+        20                              // Position
+    );
+
+    // Add submenu for Content Generation
+    add_submenu_page(
+        'intelli-content-generation',  // Parent slug (Content Generation page)
+        'Content Generation',          // Page title
+        'Content Generation',          // Menu title
+        'manage_options',              // Capability required
+        'intelli-content-generation',  // Menu slug for Content Generation page
+        'intelli_content_generation_page' // Callback function for Content Generation page
     );
 
     // Add submenu for Settings
@@ -22,6 +32,7 @@ function intelli_content_add_menu() {
         'intelli-content-settings',    // Menu slug for Settings page
         'intelli_content_settings_page' // Callback function for Settings page
     );
+
 
 }
 add_action('admin_menu', 'intelli_content_add_menu');
