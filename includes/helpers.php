71753a148cd3b0b0abfa179c508395d224584812
adminazhar <admin@azhark.com>
2024-02-15 16:19:24 +0530

    add nl 2 para

--- includes/helpers.php
+++ includes/helpers.php
@@ -1,1 +1,14 @@
 <?php
+// Converts \n\n to <p>
+function nl2p($string)
+{
+    $paragraphs = '';
+
+    foreach (explode("\n", $string) as $line) {
+        if (trim($line)) {
+            $paragraphs .= '<p>' . $line . '</p>';
+        }
+    }
+
+    return $paragraphs;
+}
